053f61982d3731348b89a4dd0a632115805be1ff
nikhil-9971 <124913952+nikhil-9971@users.noreply.github.com>
2023-04-10 11:20:54 +0530

    Add files via upload

---
+++ eventlistenerassign/mouseover/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("mouseover", mouseover);
+
+function mouseover() {
+  document.getElementById("mouseover1").innerText =
+    "I am new web developer. Please let me know if any help required. Thanks! 🥰";
+  document.getElementById("mouseover1").style.color = "black";
+}
